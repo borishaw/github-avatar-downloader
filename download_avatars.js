d2d7f0d95d188be195a0356cc2e873ecae18897a
Boris Xiao <borishaw@gmail.com>
2017-03-13 21:56:16 +0000

    step7

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,13 @@
 var request = require('request');
+var fs = require('fs');
+const credentials = require('./credentials');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  const GITHUB_USER = 'borishaw';
-  const GITHUB_TOKEN = '2aa45aaa76c254fb67affa95fb9073ee8b70a90c';
+
+  const GITHUB_USER = credentials.userName;
+  const GITHUB_TOKEN = credentials.token;
 
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
@@ -21,12 +24,40 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
+
   console.log("Errors:", err);
+
+
   var avatarArray = [];
   var counter = 0;
-  var result = JSON.parse(result);
+  result = JSON.parse(result);
   for (counter; counter < result.length; counter++){
     avatarArray.push(result[counter].avatar_url);
   }
   console.log(avatarArray);
+  return avatarArray;
 });
+
+function downloadImageByURL(url, filePath) {
+  var image = '';
+  request.get(url)
+    .on('error', function (error) {
+      console.log(error);
+    })
+    .on('response', function (response) {
+      response.on('error', function (error) {
+        console.log(error);
+      })
+        .on('data', function (chunk) {
+          image += chunk;
+        })
+        .on('end', function () {
+          console.log("Avatar image downloaded.")
+        })
+        .pipe(fs.createWriteStream(filePath).on('finish', function () {
+          console.log("Avatar image saved.");
+        }))
+    })
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
